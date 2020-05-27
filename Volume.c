1a1867d9d065d234fab3ed6284419bff81520871
Yonathan <yonathan_uchoa@hotmail.com>
2020-05-27 00:53:54 -0400

    alterei algo pequeno

--- Volume.c
+++ Volume.c
@@ -4,7 +4,7 @@
 int volume(){
 
 	int base, comprimento, altura;
-	printf("Insira a base, o comprimento e a altura:\n");
+	printf("Insira a base, o comprimento e a altura qualquer coisa:\n");
 	scanf("%d %d %d", &base, &comprimento, &altura);
 	printf ("Volume: %d\n", base*comprimento*altura);
 
